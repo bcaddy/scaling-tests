a91bcc084703495a462af2d6dab1f0673a1e2c26
Bob Caddy <r.Caddy@pitt.edu>
2023-05-04 14:20:41 -0400

    ms/256^3 cells/gpu plot done

--- scaling_plots.py
+++ scaling_plots.py
@@ -54,40 +54,46 @@ color_particles = 'C2'
 color_total     = 'w'
 color_total_old = 'grey'
 marker_size     = 4
+alpha           = 0.9
+
+ax.set_alpha(alpha)
 
 def plot(scaling_data, name, color, label):
     x = scaling_data.loc['n_proc'].to_numpy()
     y = scaling_data.loc[name].to_numpy() / (scaling_data.loc['n_steps'].to_numpy() - 1)
     x.sort()
     y.sort()
-    ax.plot(x, y, '--', c=color, alpha=0.8, marker='o', markersize=marker_size, label=label)
+    ax.plot(x, y, '--', c=color, alpha=alpha, marker='o', markersize=marker_size, label=label)
 
 scaling_data = load_data()
+plot(scaling_data, 'Total', color_total, 'Total')
 plot(scaling_data, 'MHD', color_mhd, 'MHD')
 plot(scaling_data, 'Boundaries', color_mpi, 'MPI Comm')
-plot(scaling_data, 'Total', color_total, 'Total')
 
 #2019 data for comparison
 n_procs_128_2019 = np.array([8, 64, 512, 1024, 2048, 4096, 8192, 16384])
 t_total_128_2019 = np.array([347.068, 386.369, 423.309, 439.147, 447.226, 458.162, 464.66, 481.458])
-ax.plot( n_procs_128_2019, t_total_128_2019, '--', c=color_total_old, alpha=0.8, marker='o', markersize=marker_size)
+ax.plot( n_procs_128_2019, t_total_128_2019, '--', c=color_total_old, alpha=alpha, marker='o', markersize=marker_size)
 
 font_size = 10
-ax.text(0.05, 0.95, 'Total 2019', fontsize=font_size, color=color_total_old, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
+ax.text(0.05, 0.95, 'Total Hydro 2019', fontsize=font_size, color=color_total_old, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
 
 ax.tick_params(axis='both', which='major', direction='in' )
 ax.tick_params(axis='both', which='minor', direction='in' )
 
-ax.set_xlim(1, 1E5)
-ax.set_ylim(1E-2, 1E3)
+ax.set_xlim(0.7, 1E5)
+ax.set_ylim(7E-3, 1E3)
 
 ax.set_yscale('log')
 ax.set_xscale('log')
 
-ax.set_ylabel(r'Milliseconds / 256$^3$ Cells / GPU', fontsize=font_size)
-ax.set_xlabel(r'Number of GPUs', fontsize=font_size)
+fig.suptitle('MHD Weak Scaling on Frontier', fontsize=font_size, alpha=alpha)
+ax.set_ylabel(r'Milliseconds / 256$^3$ Cells / GPU', fontsize=font_size, alpha=alpha)
+ax.set_xlabel(r'Number of GPUs', fontsize=font_size, alpha=alpha)
 
-ax.legend()
+legend = ax.legend(fontsize=0.75*font_size, frameon=False)
+for t in legend.texts:
+    t.set_alpha(alpha)
 
 output_path = pathlib.Path(__file__).parent.resolve() / 'scaling_frontier_mhd.png'
 fig.savefig(output_path, bbox_inches='tight', dpi=300)
