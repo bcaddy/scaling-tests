039438928c03786d4ea5b8e4173e4778269965d8
Bob Caddy <r.Caddy@pitt.edu>
2023-05-04 13:50:58 -0400

    First draft of plotting script

--- log_to_scaling_plot.py
+++ log_to_scaling_plot.py
@@ -1,137 +1,93 @@
 #!/usr/bin/env python3
 import sys, os
 import numpy as np
+import pandas as pd
+import pathlib
 import matplotlib.pyplot as plt
 import matplotlib
+import matplotlib.font_manager
+# Matplotlib settings
 plt.style.use('dark_background')
-
-
-import matplotlib
 matplotlib.rcParams['font.sans-serif'] = "Helvetica"
 matplotlib.rcParams['font.family'] = "sans-serif"
 matplotlib.rcParams['mathtext.fontset'] = 'cm'
 matplotlib.rcParams['mathtext.rm'] = 'serif'
 
+# Load data
+def load_data():
+    # Get paths
+    directory = str(input('Input directory name: ') or '2023-05-02-plmc')
+    data_path = pathlib.Path(__file__).parent.resolve() / 'data' / directory
+    data_dirs = sorted(pathlib.Path(data_path).glob('ranks*'))
 
+    # Setup dataframe
+    file_name = data_dirs[0] / 'run_timing.log'
+    with open(file_name, 'r') as file:
+        lines        = file.readlines()
+        header       = lines[3][1:].split()
+        scaling_data = pd.DataFrame(index=header)
 
+    # Loop through the directories and load the data
+    for path in data_dirs:
+        file_name = path / 'run_timing.log'
+        if file_name.is_file():
+            with open(file_name, 'r') as file:
+                lines  = file.readlines()
+                data   = lines[4].split()
+                scaling_data[data[0]] = pd.to_numeric(data)
+        else:
+            print(f'File: {file_name} not found.')
 
-# Globals
-filename = 'disk_scaling.log'
-plot_grav_over_logN = False
+    return scaling_data
 
-combos = {}
-combos['Hydro'] = ['Hydro','Calc_dt']
-combos['MPI comm'] = ['Boundaries','Pot_Boundaries','Part_Boundaries','Part_Dens_Transf']
-combos['Poisson'] = ['Grav_Potential']
-combos['Particles'] = ['Part_Density','Advance_Part_1','Advance_Part_2']
-
-def filename_to_table(filename):
-
-    data = np.loadtxt(filename,skiprows=3,comments=['#'])
-    with open(filename,'r') as ofile:
-        lines = ofile.readlines()
-        #print(lines)
-        header = lines[3].split()
-    table = {}
-    for i in range(len(header)):
-        table[header[i]] = data[:,i]
-
-    print(table)
-    return table
-
-def access(table, item):
-    # If item is a string, plot its average time
-    if item in combos:
-        total_time = 0
-        for subitem in combos[item]:
-            total_time += table[subitem]
-    elif item in table:
-        total_time = table[item]
-    else:
-        print('Error: missing key:', item)
-        return
-    x = table['#n_proc']
-    y = total_time / table['n_steps']
-    order = np.argsort(x)
-    return x[order], y[order]
-
-
-
-
-
+# Instantiate Plot
 fig = plt.figure(0)
 fig.set_size_inches(4,3)
 fig.clf()
 ax = plt.gca()
 
-c_hydro = 'C0'
-c_mpi = 'C4'
-c_grav = 'C3'
-c_particles = 'C2'
-c_total = 'w'
-c_total_old = 'grey'
+# Set plot settings
+color_mhd       = 'C0'
+color_mpi       = 'C4'
+color_grav      = 'C3'
+color_particles = 'C2'
+color_total     = 'w'
+color_total_old = 'grey'
+marker_size     = 4
 
-ms = 4
+def plot(scaling_data, name, color, label):
+    x = scaling_data.loc['n_proc'].to_numpy()
+    y = scaling_data.loc[name].to_numpy() / (scaling_data.loc['n_steps'].to_numpy() - 1)
+    x.sort()
+    y.sort()
+    ax.plot(x, y, '--', c=color, alpha=0.8, marker='o', markersize=marker_size, label=label)
 
-
-frontier_table = filename_to_table(filename)
-
-def plot(name, color):
-    x, y = access(frontier_table,name)
-    ax.plot(x, y, '--', c=color, alpha=0.8, marker='o', markersize=ms)
-
-
-
-plot('Hydro', c_hydro)
-plot('MPI comm', c_mpi)
-plot('Poisson', c_grav)
-plot('Particles', c_particles)
-plot('Total', c_total)
+scaling_data = load_data()
+plot(scaling_data, 'MHD', color_mhd, 'MHD')
+plot(scaling_data, 'Boundaries', color_mpi, 'MPI Comm')
+plot(scaling_data, 'Total', color_total, 'Total')
 
 #2019 data for comparison
 n_procs_128_2019 = np.array([8, 64, 512, 1024, 2048, 4096, 8192, 16384])
 t_total_128_2019 = np.array([347.068, 386.369, 423.309, 439.147, 447.226, 458.162, 464.66, 481.458])
-ax.plot( n_procs_128_2019, t_total_128_2019, '--', c=c_total_old, alpha=0.8, marker='o', markersize=ms)
+ax.plot( n_procs_128_2019, t_total_128_2019, '--', c=color_total_old, alpha=0.8, marker='o', markersize=marker_size)
 
-
-
-fs = 10
-ax.text(0.85, 0.47, 'Hydro', fontsize=fs, color=c_hydro, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-
-ax.text(0.05, 0.4, 'MPI comm', fontsize=fs, color=c_mpi, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-
-ax.text(0.05, 0.16, 'Particles', fontsize=fs, color=c_particles, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-
-
-
-if plot_grav_over_logN:
-  ax.text(0.05, 0.37, 'Poisson / log(N)', fontsize=fs, color=c_grav, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-  ax.text(0.05, 0.72, 'Total 2023', fontsize=fs, color=c_total, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-
-else:
-  ax.text(0.3, 0.62, 'Poisson', fontsize=fs, color=c_grav, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-  ax.text(0.05, 0.75, 'Total 2023', fontsize=fs, color=c_total, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-
-ax.text(0.05, 0.95, 'Total 2019', fontsize=fs, color=c_total_old, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-
-
-ax.set_xlim(6, 100000)
-ax.set_ylim(0.1, 1000)
-ax.set_yscale('log')
+font_size = 10
+ax.text(0.05, 0.95, 'Total 2019', fontsize=font_size, color=color_total_old, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
 
 ax.tick_params(axis='both', which='major', direction='in' )
 ax.tick_params(axis='both', which='minor', direction='in' )
 
+ax.set_xlim(1, 1E5)
+ax.set_ylim(1E-2, 1E3)
 
-fs = 10
-ax.set_ylabel( r'Milliseconds / 256$^3$ Cells / GPU', fontsize=fs)
-ax.set_xlabel( r'Number of GPUs', fontsize=fs)
+ax.set_yscale('log')
 ax.set_xscale('log')
 
-output_dir = './'
+ax.set_ylabel(r'Milliseconds / 256$^3$ Cells / GPU', fontsize=font_size)
+ax.set_xlabel(r'Number of GPUs', fontsize=font_size)
 
+ax.legend()
 
-
-if plot_grav_over_logN: fileName = output_dir + 'scaling_frontier_adiabatic_2023_log_new_logN.png'
-else: fileName = output_dir + 'scaling_frontier_adiabatic_2023_dark.png'
-fig.savefig(  fileName ,  bbox_inches='tight', dpi=300)
+output_path = pathlib.Path(__file__).parent.resolve() / 'scaling_frontier_mhd.png'
+fig.savefig(output_path, bbox_inches='tight', dpi=300)
