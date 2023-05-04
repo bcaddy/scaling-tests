612b46111b473396cf51e685c62d2463a58fcb69
Bob Caddy <r.Caddy@pitt.edu>
2023-05-04 15:18:32 -0400

    cells per second per gpu plots

--- scaling_plots.py
+++ scaling_plots.py
@@ -34,66 +34,119 @@ def load_data():
             with open(file_name, 'r') as file:
                 lines  = file.readlines()
                 data   = lines[4].split()
-                scaling_data[data[0]] = pd.to_numeric(data)
+                scaling_data[int(data[0])] = pd.to_numeric(data)
         else:
             print(f'File: {file_name} not found.')
 
+    scaling_data = scaling_data.reindex(sorted(scaling_data.columns), axis=1)
     return scaling_data
 
-# Instantiate Plot
-fig = plt.figure(0)
-fig.set_size_inches(4,3)
-fig.clf()
-ax = plt.gca()
+def Scaling_Plot(scaling_data, y_title, filename, plot, old_data, old_label_position, xlims, ylims, skipped_fields=[]):
+    # Instantiate Plot
+    fig = plt.figure(0)
+    fig.set_size_inches(4,3)
+    fig.clf()
+    ax = plt.gca()
 
-# Set plot settings
-color_mhd       = 'C0'
-color_mpi       = 'C4'
-color_grav      = 'C3'
-color_particles = 'C2'
-color_total     = 'w'
-color_total_old = 'grey'
-marker_size     = 4
-alpha           = 0.9
+    # Set plot settings
+    color_mhd       = 'C0'
+    color_mpi       = 'C4'
+    color_grav      = 'C3'
+    color_particles = 'C2'
+    color_total     = 'w'
+    color_total_old = 'grey'
+    marker_size     = 4
+    alpha           = 0.9
 
-ax.set_alpha(alpha)
+    ax.set_alpha(alpha)
 
-def plot(scaling_data, name, color, label):
+    if 'Total' not in skipped_fields:
+        ax = plot(scaling_data, 'Total', color_total, 'Total', ax, alpha, marker_size)
+    if 'MHD' not in skipped_fields:
+        ax = plot(scaling_data, 'MHD', color_mhd, 'MHD', ax, alpha, marker_size)
+    if 'MPI Comm' not in skipped_fields:
+        ax = plot(scaling_data, 'Boundaries', color_mpi, 'MPI Comm', ax, alpha, marker_size)
+
+    #2019 data for comparison
+    n_procs_128_2019 = np.array([8, 64, 512, 1024, 2048, 4096, 8192, 16384])
+    t_total_128_2019 = np.array([347.068, 386.369, 423.309, 439.147, 447.226, 458.162, 464.66, 481.458])
+    if (old_data == 'rescale'):
+        t_total_128_2019 = (256**3) / t_total_128_2019 * 1000
+    ax.plot( n_procs_128_2019, t_total_128_2019, '--', c=color_total_old, alpha=alpha, marker='o', markersize=marker_size)
+
+    font_size = 10
+    ax.text(old_label_position[0], old_label_position[1],
+            'Total Hydro 2019',
+            fontsize=font_size,
+            color=color_total_old,
+            horizontalalignment='left',
+            verticalalignment='center',
+            transform=ax.transAxes)
+
+    ax.tick_params(axis='both', which='major', direction='in' )
+    ax.tick_params(axis='both', which='minor', direction='in' )
+
+    ax.set_xlim(xlims[0], xlims[1])
+    ax.set_ylim(ylims[0], ylims[1])
+
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+
+    fig.suptitle('MHD Weak Scaling on Frontier', fontsize=font_size, alpha=alpha)
+    ax.set_ylabel(y_title, fontsize=font_size, alpha=alpha)
+    ax.set_xlabel(r'Number of GPUs', fontsize=font_size, alpha=alpha)
+
+    legend = ax.legend(fontsize=0.75*font_size, frameon=False)
+    for t in legend.texts:
+        t.set_alpha(alpha)
+
+    output_path = pathlib.Path(__file__).parent.resolve() / filename
+    fig.savefig(output_path, bbox_inches='tight', dpi=300)
+
+def ms_per_256_per_gpu(scaling_data, name, color, label, ax, alpha, marker_size):
     x = scaling_data.loc['n_proc'].to_numpy()
     y = scaling_data.loc[name].to_numpy() / (scaling_data.loc['n_steps'].to_numpy() - 1)
-    x.sort()
-    y.sort()
+
     ax.plot(x, y, '--', c=color, alpha=alpha, marker='o', markersize=marker_size, label=label)
 
-scaling_data = load_data()
-plot(scaling_data, 'Total', color_total, 'Total')
-plot(scaling_data, 'MHD', color_mhd, 'MHD')
-plot(scaling_data, 'Boundaries', color_mpi, 'MPI Comm')
+    return ax
 
-#2019 data for comparison
-n_procs_128_2019 = np.array([8, 64, 512, 1024, 2048, 4096, 8192, 16384])
-t_total_128_2019 = np.array([347.068, 386.369, 423.309, 439.147, 447.226, 458.162, 464.66, 481.458])
-ax.plot( n_procs_128_2019, t_total_128_2019, '--', c=color_total_old, alpha=alpha, marker='o', markersize=marker_size)
+def cells_per_second_per_gpu(scaling_data, name, color, label, ax, alpha, marker_size):
+    x = (scaling_data.loc['n_proc'].to_numpy())
 
-font_size = 10
-ax.text(0.05, 0.95, 'Total Hydro 2019', fontsize=font_size, color=color_total_old, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
+    cells_per_gpu = ( scaling_data.loc['nx'].to_numpy()
+                    * scaling_data.loc['ny'].to_numpy()
+                    * scaling_data.loc['nz'].to_numpy()) / x
 
-ax.tick_params(axis='both', which='major', direction='in' )
-ax.tick_params(axis='both', which='minor', direction='in' )
+    avg_time_step = scaling_data.loc[name].to_numpy() / (scaling_data.loc['n_steps'].to_numpy() - 1)
+    avg_time_step /= 1000 # convert to seconds from ms
 
-ax.set_xlim(0.7, 1E5)
-ax.set_ylim(7E-3, 1E3)
+    y = cells_per_gpu / avg_time_step
 
-ax.set_yscale('log')
-ax.set_xscale('log')
+    ax.plot(x, y, '--', c=color, alpha=alpha, marker='o', markersize=marker_size, label=label)
 
-fig.suptitle('MHD Weak Scaling on Frontier', fontsize=font_size, alpha=alpha)
-ax.set_ylabel(r'Milliseconds / 256$^3$ Cells / GPU', fontsize=font_size, alpha=alpha)
-ax.set_xlabel(r'Number of GPUs', fontsize=font_size, alpha=alpha)
+    return ax
 
-legend = ax.legend(fontsize=0.75*font_size, frameon=False)
-for t in legend.texts:
-    t.set_alpha(alpha)
+def main():
+    scaling_data = load_data()
 
-output_path = pathlib.Path(__file__).parent.resolve() / 'scaling_frontier_mhd.png'
-fig.savefig(output_path, bbox_inches='tight', dpi=300)
+    Scaling_Plot(scaling_data=scaling_data,
+                 y_title=r'Milliseconds / 256$^3$ Cells / GPU',
+                 filename='ms_per_gpu',
+                 plot=ms_per_256_per_gpu,
+                 old_data='keep',
+                 old_label_position=[0.03, 0.96],
+                 xlims=[0.7, 1E5],
+                 ylims=[7E-3, 1E3])
+
+    Scaling_Plot(scaling_data=scaling_data,
+                 y_title=r'Cells / Second / GPU',
+                 filename='cells_per_second',
+                 plot=cells_per_second_per_gpu,
+                 old_data='rescale',
+                 old_label_position=[0.65, 0.20],
+                 xlims=[0.7, 1E5],
+                 ylims=[1E7, 1E9],
+                 skipped_fields=['Total', 'MPI Comm'])
+
+main()
